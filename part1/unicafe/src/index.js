020e700801ae94eca260c82d894f0bee8015e0e4
kaung khant thar <kaungkhantthar@ucsm.edu.mm>
2020-10-18 17:40:59 +0630

    Solved 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,6 +6,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
 
   return (
     <div>
@@ -20,6 +21,12 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+
+      <p>
+        average {total === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / total}
+      </p>
+      <p>positive {total === 0 ? 0 : (good / total) * 100} % </p>
     </div>
   );
 };
