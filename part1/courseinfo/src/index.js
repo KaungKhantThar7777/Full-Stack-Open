214e250853c95e63ce57b279dc43dddd71fc7042
kaung khant thar <kaungkhantthar@ucsm.edu.mm>
2020-10-18 16:04:53 +0630

    Fixed 1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function Header({ course }) {
-  return <h1>{course}</h1>;
+  return <h1>{course.name}</h1>;
 }
 function Content({ parts }) {
   return (
@@ -28,27 +28,29 @@ function Part({ part: { name, exercises } }) {
   );
 }
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
