d756e78b442c7a5c0c3df5c1989d53c3a03b9d41
kaung khant thar <kaungkhantthar@ucsm.edu.mm>
2020-10-19 09:36:33 +0630

    Solved 2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "123-456-789" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
+  };
+  const handlePhoneChange = (e) => {
+    setNewPhone(e.target.value);
   };
 
   const handleSubmit = (e) => {
@@ -14,8 +20,9 @@ const App = () => {
     if (isAlreadyExist) {
       alert(`${newName} is already added to the phonebook`);
     } else {
-      setPersons(persons.concat({ name: newName }));
+      setPersons(persons.concat({ name: newName, number: newPhone }));
       setNewName("");
+      setNewPhone("");
     }
   };
 
@@ -27,12 +34,17 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newPhone} onChange={handlePhoneChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({ name }) => (
-        <p key={name}>{name}</p>
+      {persons.map(({ name, number }) => (
+        <p key={name}>
+          {name} {number}
+        </p>
       ))}
     </div>
   );
