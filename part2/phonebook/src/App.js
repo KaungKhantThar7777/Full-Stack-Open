4a06eaea6c898c6c6150e80c106d9cbad58e06e7
kaung khant thar <kaungkhantthar@ucsm.edu.mm>
2020-10-19 09:41:25 +0630

    Solved 2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "123-456-789" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
+  const [filter, setFilter] = useState("");
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
@@ -26,10 +30,21 @@ const App = () => {
     }
   };
 
+  const personsToShow = filter
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </div>
       <form onSubmit={handleSubmit}>
+        <h2>Add a new </h2>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
@@ -41,7 +56,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({ name, number }) => (
+      {personsToShow.map(({ name, number }) => (
         <p key={name}>
           {name} {number}
         </p>
