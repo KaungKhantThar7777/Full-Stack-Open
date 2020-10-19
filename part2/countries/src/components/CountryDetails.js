9e87dacd306938e78adedc459e5be26b6e9a1451
kaung khant thar <kaungkhantthar@ucsm.edu.mm>
2020-10-19 13:39:17 +0630

    Solved 2.14*: Data for countries, step3

--- part2/countries/src/components/CountryDetails.js
+++ part2/countries/src/components/CountryDetails.js
@@ -1,7 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 const CountryDetails = ({ country }) => {
   const { name, capital, population, flag, languages } = country;
+  const [weather, setWeather] = React.useState(null);
+  React.useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_WEATHER_API_KEY}&query=${capital}`
+      )
+      .then(({ data }) => setWeather(data.current))
+      .catch((err) => console.log(err));
+  }, [capital]);
   return (
     <div>
       <h2>{name}</h2>
@@ -15,6 +25,16 @@ const CountryDetails = ({ country }) => {
           <li key={language.name}>{language.name}</li>
         ))}
       </ul>
+      {weather && (
+        <div>
+          <h3>Weather in {capital}</h3>
+          <p>temperature: {weather.temperature} C</p>
+          <img src={weather.weather_icons[0]} alt={capital} />
+          <p>
+            wind: {weather.wind_speed} mph direction {weather.wind_direction}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
